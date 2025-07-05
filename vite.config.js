072a7a154e332d616fe104f50223fc6c26bd445e
Saqib Islam <saaqi.grw@gmail.com>
2025-07-06 00:54:54 +0500

    update dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,50 +1,14 @@
-const IN_PRODUCTION = process.env.NODE_ENV === 'production';
-import { defineConfig } from 'vite';
-import { sveltekit } from '@sveltejs/kit/vite';
-import webfontDownload from 'vite-plugin-webfont-dl';
-// import { purgeCSSPlugin } from '@fullhuman/postcss-purgecss';
-import htmlPurge from 'vite-plugin-purgecss';
-import autoprefixer from 'autoprefixer';
 import cssnano from 'cssnano';
+import { defineConfig } from 'vite';
+import autoprefixer from 'autoprefixer';
 const bootstrap = 'node_modules/bootstrap';
+import { sveltekit } from '@sveltejs/kit/vite';
+const IN_PRODUCTION = process.env.NODE_ENV === 'production';
+import { purgeCSSPlugin } from '@fullhuman/postcss-purgecss';
 
 export default defineConfig({
 	plugins: [
 		sveltekit(),
-		htmlPurge({
-			content: [
-				'./src/app.html',
-				'./src/**/*.js',
-				'./src/**/*.svelte',
-				// --- Import only the required components.
-				// `${bootstrap}/js/dist/alert.js`,
-				// `${bootstrap}/js/dist/base-component.js`,
-				// `${bootstrap}/js/dist/button.js`,
-				// `${bootstrap}/js/dist/carousel.js`,
-				`${bootstrap}/js/dist/collapse.js`,
-				// `${bootstrap}/js/dist/dropdown.js`,
-				`${bootstrap}/js/dist/modal.js`
-				// `${bootstrap}/js/dist/offcanvas.js`,
-				// `${bootstrap}/js/dist/popover.js`,
-				// `${bootstrap}/js/dist/scrollspy.js`,
-				// `${bootstrap}/js/dist/tab.js`,
-				// `${bootstrap}/js/dist/toast.js`,
-				// `${bootstrap}/js/dist/tooltip.js`,
-			],
-			safelist: [/svelte-/, /modal-/],
-			defaultExtractor(content) {
-				const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '');
-				return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || [];
-			},
-			keyframes: true,
-			variables: true
-		}),
-
-		/* ## Download Google Fonts and attach them with production build for offline use */
-		IN_PRODUCTION &&
-			webfontDownload([
-				'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap'
-			])
 	],
 
 	css: {
@@ -54,14 +18,26 @@ export default defineConfig({
 			}
 		},
 		postcss: {
-			plugins: [
-				// --- CSSNano is a modern CSS minifier based on the PostCSS ecosystem.
-				cssnano({
-					preset: ['default', { discardComments: { removeAll: true } }]
-				}),
-				// --- Autoprefixer is used to add vendor prefixes to CSS rules using values from "Can I Use".
-				autoprefixer
-			]
+			plugins: IN_PRODUCTION
+				? [
+					purgeCSSPlugin({
+						content: [
+							'./src/app.html',
+							'./src/**/*.{svelte,ts,js}',
+							`${bootstrap}/js/dist/modal.js`,
+							`${bootstrap}/js/dist/collapse.js`
+						],
+						safelist: [/svelte-/, /modal-/],
+						defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+						keyframes: true,
+						variables: true
+					}),
+					cssnano({
+						preset: ['default', { discardComments: { removeAll: true } }]
+					}),
+					autoprefixer()
+				]
+				: []
 		}
 	},
 
